2c6f6103592912bd2b930fb7c7f48e7d5345c85e
procrastination <henry@rently.com>
2019-12-09 14:53:02 -0800

    configure Enzyme in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import { render } from '@testing-library/react';
 import App from './App';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
