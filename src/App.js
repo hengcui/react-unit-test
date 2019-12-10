7328b597fee4903e4e086b3acd53752bf4d4223c
procrastination <henry@rently.com>
2019-12-10 15:17:41 -0800

    1. set up five test cases for increment counter 2. extract setup, findByTestAttr function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div data-test="component-app">
+        <div data-test="counter">Currently couter display</div>
+        <button data-test="button-increment">
+          Increment
+        </button>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
-import { render } from '@testing-library/react';
 import App from './App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-test('renders learn react link', () => {
-  const wrapper = shallow(<App />);
-  console.log(wrapper.debug());
-  expect(wrapper).toBeTruthy();
+const setup = (props = {}, state = {}) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+
+  return wrapper;
+}
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+}
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, "component-app");
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increment button', () => {
+  const wrapper = setup();
+  const incrementButton = findByTestAttr(wrapper, "button-increment");
+  expect(incrementButton.length).toBe(1);
+});
+
+test('renders counter display', () => {
+  const wrapper = setup();
+  const counter = findByTestAttr(wrapper, "counter");
+  expect(counter.length).toBe(1);
+});
+
+test('renders counter start at 0', () => {
+
+});
+
+test('renders clicking button increments counter display', () => {
+
 });
